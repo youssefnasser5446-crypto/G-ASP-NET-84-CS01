66c8f865f65efec24de7dde3307975833a63946c
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-17 16:16:42 +0300

    Question01

--- session02_Assignment/Program.cs
+++ session02_Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            /*
+             Create a Book class with a Title (string) and Pages (int).
+            Create a Book object and store it in a variable of type object.
+            Print it. 
+             */
+
+            //object Bk = new Book() { title = "C# Basecs", pages = 32 };
+            //Console.WriteLine($"The obj is {Bk}");
+            #endregion
+
+           
         }
     }
 }
