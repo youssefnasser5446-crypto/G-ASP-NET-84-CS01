e3607c9cd287e40ec2684bf0329e4fec02f4a1ef
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-07-17 16:13:19 +0300

    Add project files.

---
+++ session02_Assignment/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session02_Assignment
+{
+    internal class Book
+    {
+        public int pages;
+        public string title;
+
+        public string Title { set; get; }
+        public int Pages { set; get; }
+    }
+}
---
+++ session02_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace session02_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
